a47721320bc783751ea78ca08b11250df1d7829c
hipparagi9087 <105551468+hipparagi9087@users.noreply.github.com>
2022-05-14 19:25:16 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Positive {
+		public static void main (String args[])
+		{
+			int a=46;
+			if(a>=0)
+			{
+				 System.out.println(a+ " is positive number");
+			}
+			else
+			{
+				 System.out.println(a+" is negative number");
+			}
+		}
+
+}
